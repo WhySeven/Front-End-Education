b77202d43ee494dd05e580aa835457587ee398e4
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2020-06-06 06:55:12 +0300

    like-button toggle radio-button checkbox

---
+++ #source/common-blocks/like-button/like-button.js
@@ -0,0 +1,35 @@
+$('#like_1').click(function(){
+    let count = parseInt($('#like__count_1').text());
+    if ($(this).hasClass('active')){
+        count++;
+        $('#like__count_1').text(count);
+        $('.like__icon_1').text('favorite_border');
+    }
+    else{
+        count--;
+        $('#like__count_1').text(count);
+        $('.like__icon_1').text('favorite');
+    }
+    console.log(count);
+    
+});
+
+$('#like_2').click(function(){
+    let count = parseInt($('#like__count_2').text());
+    if ($(this).hasClass('active')){
+        count++;
+        $('#like__count_2').text(count);
+        $('.like__icon_2').text('favorite_border');
+    }
+    else{
+        count--;
+        $('#like__count_2').text(count);
+        $('.like__icon_2').text('favorite');
+    }
+    console.log(count);
+
+});
+
+$('.like').click(function(){
+    $(this).toggleClass("active");
+});
