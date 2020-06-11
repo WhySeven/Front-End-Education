93b55918996499d1f84391336050bebdbb697c28
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2020-06-11 22:01:32 +0300

    11.06.2020 dropdown, grid rework

--- #source/common-blocks/dropdown/dropdown.js
+++ #source/common-blocks/dropdown/dropdown.js
@@ -1,52 +1,62 @@
 function create_dropdown(){
-    $(".dropdown__expand-button").click(function(){
-        $(".dropdown__content").toggle();
-        $(".dropdown__expand-button").toggleClass('border-radius');
-        $(".dropdown__expand-button").toggleClass('border-radius_all');
-    });
-    let i = 0;
-    $(".dropdown__button-block").each(function(){
-        let buttonblock = ".dropdown__button-block:eq("+i+")";
-        $(buttonblock + " .dropdown__button:eq(0)").click(function(){
-            let txt = parseInt($(buttonblock + " h3").text());
-            if(txt>0){
-                $(buttonblock + " h3").text(txt-1);  
-            }
+    $(".dropdown").each(function(i){
+        let dd = ".dropdown:eq("+i+") ";
+        $(dd + ".dropdown__expand-button").click(function(){
+            $(dd + ".dropdown__content").toggle();
+            $(dd + ".dropdown__expand-button").toggleClass('border-radius');
+            $(dd + ".dropdown__expand-button").toggleClass('border-radius_all');
         });
-        $(buttonblock + " .dropdown__button:eq(1)").click(function(){
-            let txt = parseInt($(buttonblock + " h3").text());
-            $(buttonblock + " h3").text(txt+1);
+        $(dd + ".dropdown__button_enter").click(function(){
+            let count = 0;
+            let mod;
+            $(dd + ".dropdown__button-block .dropdown__count").each(function(){
+                count+=parseInt($(this).text());
+            });
+            if(count == 1)
+                mod = " гость";
+            else if(count == 0)
+                mod = " гостей";
+            else if(count < 5)
+                mod = " гостя";
+            else
+                mod = " гостей";
+            $(dd + ".dropdown__label:eq(0)").text(count + mod);
+            $(dd + ".dropdown__button-block__bottom").addClass("space-between");
+            $(dd + ".dropdown__button-block__bottom").removeClass("flex-end");
         });
-        i++;
-    });
-    $(".dropdown__button_enter").click(function(){
-        let count = 0;
-        let mod;
-        $(".dropdown__button-block .dropdown__count").each(function(){
-            count+=parseInt($(this).text());
+        $(dd + ".dropdown__button_clear").click(function(){
+            $(dd + ".dropdown__label:eq(0)").text('Сколько гостей');
+            $(dd  +".dropdown__button-block").each(function(j){
+                let bb = dd + ".dropdown__button-block:eq("+j+")";
+                $(bb + " h3").text(0); 
+            });
+            $(dd + '.dropdown__button-block__bottom').removeClass('space-between');
+            $(dd + '.dropdown__button-block__bottom').addClass('flex-end');
         });
-        if(count == 1)
-            mod = " гость";
-        else if(count == 0)
-            mod = " гостей";
-        else if(count < 5)
-            mod = " гостя";
-        else
-            mod = " гостей";
-        $(".dropdown__label:eq(0)").text(count + mod);
-        $('.dropdown__button-block__bottom').addClass('space-between');
-        $('.dropdown__button-block__bottom').removeClass('flex-end');
+        if($(dd + ".dropdown__content").hasClass("exp")){
+            $(dd + ".dropdown__content").toggle();
+            $(dd + ".dropdown__expand-button").toggleClass('border-radius');
+            $(dd + ".dropdown__expand-button").toggleClass('border-radius_all');
+        }
     });
-    $(".dropdown__button_clear").click(function(){
-        $(".dropdown__label:eq(0)").text('Сколько гостей');
-        i=0;
-        $(".dropdown__button-block").each(function(){
-            let buttonblock = ".dropdown__button-block:eq("+i+")";
-            $(buttonblock + " h3").text(0); 
-            i++;
+    $(".dropdown__button-block").each(function(i){
+        let bb = ".dropdown__button-block:eq("+i+")";
+        $(bb + " .dropdown__button:eq(0)").click(function(){
+            let txt = parseInt($(bb + " h3").text());
+            if(txt>0)
+                $(bb + " h3").text(txt-1);
         });
-        $('.dropdown__button-block__bottom').removeClass('space-between');
-        $('.dropdown__button-block__bottom').addClass('flex-end');
+        $(bb + " .dropdown__button:eq(1)").click(function(){
+            let txt = parseInt($(bb + " h3").text());
+            $(bb + " h3").text(txt+1);
+        });
     });
+    $(".dropdown_ap").each(function(i){
+        let ap = ".dropdown_ap:eq("+i+") ";
+        $(ap + ".dropdown__button").click(function(){
+            $(ap + ".dropdown__label:eq(0)").text($(ap + ".dropdown__count:eq(0)").text() + " cпальни, " + $(ap + ".dropdown__count:eq(1)").text() + " кровати...");
+        });
+    });
+    
 }
 create_dropdown();
--- #source/common-blocks/dropdown/dropdown1.js
+++
@@ -1,7 +0,0 @@
-function create_dropdown(){
-    $(".dropdown").each(function(i){
-        console.log(i);
-        $(".dropdown:eq(0) .dropdown__content").toggle();
-    });
-}
-create_dropdown();
